1bad337895468005b55850033b0828191bf43b96
ArisGoi <aris.goi97@gmail.com>
2022-10-04 23:32:07 +0200

    Card Design Selector + Folder order

---
+++ js/style-selector.js
@@ -0,0 +1,9 @@
+
+
+
+
+document.getElementById('card-design-select').addEventListener("change", (ev)=>{
+
+    document.getElementById('field').setAttribute('card-design', ev.target.value);
+
+} );
